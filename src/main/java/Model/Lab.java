4a3beb6bc411a6af96f249049e441a2c9169c7ea
NamitKumar16 <69860507+NamitKumar16@users.noreply.github.com>
2021-05-24 17:35:13 +0530

    Model
    Created Model classes and getter setter methods

---
+++ src/main/java/Model/Lab.java
@@ -0,0 +1,41 @@
+package Model;
+
+public class Lab {
+	public String name;
+	public String address;
+	public String labID;
+	public String contactno;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getLabID() {
+		return labID;
+	}
+
+	public void setLabID(String labID) {
+		this.labID = labID;
+	}
+
+	public String getContactno() {
+		return contactno;
+	}
+
+	public void setContactno(String contactno) {
+		this.contactno = contactno;
+	}
+
+}
