4f4580ba782cb24d159950267e56618c2f6d49d8
NamitKumar16 <69860507+NamitKumar16@users.noreply.github.com>
2021-06-15 22:52:10 +0530

    Final

--- src/main/java/com/project/RestController/UserController.java
+++ src/main/java/com/project/RestController/UserController.java
@@ -1,28 +1,23 @@
 package com.project.RestController;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.project.DAO.UserDAO1;
-import com.project.DAO.UserDao;
-import com.project.api.User;
-import com.project.service.UserService;
+import com.project.DAO.PatientDAO;
+import com.project.api.Patient;
 
 @RestController
 public class UserController {
 
-	
-	
 	@Autowired
-	private UserDAO1 userDao; 
-	
-	@RequestMapping(path="/adduser",method = RequestMethod.POST)
-	public String addUser(User user ) {
-		return userDao.addUser(user);
-		
+	private PatientDAO patientDao;
+
+	@RequestMapping(path = "/addpatient", method = RequestMethod.POST)
+	public String addpatient(Patient patient) {
+		return patientDao.addpatient(patient);
+
 	}
-	
+
 }
